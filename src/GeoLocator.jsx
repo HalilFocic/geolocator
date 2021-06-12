b145dba0c5308f43849c6d55c10b37ef2227884b
Halil Focic <halil.focic@gmail.com>
2021-06-12 19:47:45 +0200

    top part done

--- src/GeoLocator.jsx
+++ src/GeoLocator.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+let placeholderValue = "Search for any IP address or domain";
 const GeoLocator = () => {
-  return <div>Gelocator</div>;
+  const [address, setAddress] = useState("");
+
+  // Send requrest to APi to retrieve data about the ip address that was entered
+  const searchAddress = () => {
+    alert(address);
+  };
+
+  return (
+    <div className="App">
+      <div className="top-part">
+        <div className="app-title">IP Address Tracker</div>
+        <div className="geo-form">
+          <input
+            type="text"
+            className="geo-input"
+            placeholder={placeholderValue}
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <button className="geo-btn" onClick={() => searchAddress()}>
+            {">"}
+          </button>
+        </div>
+      </div>
+      <div className="geo-map">Mapa</div>"
+    </div>
+  );
 };
 
 export default GeoLocator;
