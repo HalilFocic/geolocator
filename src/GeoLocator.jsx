5e50d67626b17c5c70c9c5afb0a1b8af802658c0
Halil Focic <halil.focic@gmail.com>
2021-06-12 18:16:12 +0200

    init

---
+++ src/GeoLocator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeoLocator = () => {
+  return <div>Gelocator</div>;
+};
+
+export default GeoLocator;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import GeoLocator from "./GeoLocator";
+ReactDOM.render(
+  <React.StrictMode>
+    <GeoLocator />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
