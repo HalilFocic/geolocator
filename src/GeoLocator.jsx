e7e9db511c9b36dfe089ce89c8d91a648e4c8d9b
Halil Focic <halil.focic@gmail.com>
2021-06-13 18:20:40 +0200

    working

--- src/GeoLocator.jsx
+++ src/GeoLocator.jsx
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from "react";
-import Map from "./Map";
+import React, { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import markerIcon from "./images/marker.svg";
 let placeholderValue = "Search for any IP address or domain";
 const GeoLocator = () => {
+  const [hasSearched, setHasSearched] = useState(false);
   const [address, setAddress] = useState("");
-
-  // Send requrest to APi to retrieve data about the ip address that was entered
+  const [viewport, setViewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "70vh",
+    zoom: 10,
+  });
+  const [markerData, setMarkerData] = useState({
+    lat: 45.4211,
+    lng: -75.6903,
+  });
   const searchAddress = async () => {
     const res = await fetch(
-      `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_KEY}&ipAddress=${address}`
+      `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_ADRESS_KEY}&ipAddress=${address}`
     );
     const data = await res.json();
-    console.log(data);
+    let newViewPort = { ...viewport };
+    newViewPort.latitude = data.location.lat;
+    newViewPort.longitude = data.location.lng;
+    setMarkerData({ lat: data.location.lat, lng: data.location.lng });
+    setHasSearched(true);
+    setViewport(newViewPort);
   };
-  console.log(process.env.REACT_APP_KEY);
   return (
     <div className="App">
       <div className="top-part">
@@ -30,8 +45,21 @@ const GeoLocator = () => {
           </button>
         </div>
       </div>
-      <div id="mapid">
-        <Map />
+      <div>
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_KEY}
+          onViewportChange={(newPort) => {
+            setViewport(newPort);
+          }}
+          mapStyle="mapbox://styles/halilfocic/ckpvdhjl40tef18q67obqvn6t"
+        >
+          {hasSearched && (
+            <Marker latitude={markerData.lat} longitude={markerData.lng}>
+              <img src={markerIcon} alt="markercic" width="30px" />
+            </Marker>
+          )}
+        </ReactMapGL>
       </div>
     </div>
   );
